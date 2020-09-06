2270ba75e6dcf10912512321e6c0f78f08bc42b8
Gunnar Bachelor <gunnarbachelor@localhost.localdomain>
2020-09-06 17:33:45 -0500

    Completed rough homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,17 @@ export default function Index() {
    <div className={"indexWrapper"}>
 
       <div className={"nav"}>
+       <a href={"https://www.instagram.com/olivewrztartan"}>Instagram</a>
+       <a href={"https://twitter.com/devilwrztartan"}>Twitter</a>
+       <a href={"https://vm.tiktok.com/ZMJS78bUH"}>Tiktok</a>
       </div>
-      <div className={"collage"}>
+      <div className={"collageWrapper"}>
+
+        <div className={"collage"}>  
+          <div className={"blogTitle"}><Link to="blog"><h2>Blog</h2></Link></div> 
+          <div className={"olivia"}><img src="https://dl.dropboxusercontent.com/s/nqwesknwszrzkfw/olivia_collage.png?dl=0" alt="Olivia" /></div>
+        </div>
       </div>
-      <div className={"indexFooter"}>
-      </div>
-
-
-
    </div>
   )
 }
