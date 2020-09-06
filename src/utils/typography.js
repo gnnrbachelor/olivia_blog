7203038500064e72acc515dd54cff027a5c21568
Gunnar Bachelor <gunnarbachelor@localhost.localdomain>
2020-09-06 13:09:06 -0500

    Changed typography.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,8 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography'
+import judahTheme from 'typography-theme-judah'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const typography = new Typography(judahTheme)
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
